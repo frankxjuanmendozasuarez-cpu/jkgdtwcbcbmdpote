656de576dbef804d6f8f7e70480e8925a8474fff
frankxjuanmendozasuarez-cpu <frankxjuanmendozasuarez@gmail.com>
2025-12-01 16:27:24 +0100

    Add files via upload

---
+++ trl.js
@@ -0,0 +1,2 @@
+// trl.js
+const webhookUrl = 'https://discord.com/api/webhooks/1440402775656759317/N0veSLtaPssp2iFYFyuQ4uS5BaG7dQITmilDbiKumu3q8Pp47gZojKBgcnOLbOUZGWEF'; // Reemplaza esto con tu Webhook real
